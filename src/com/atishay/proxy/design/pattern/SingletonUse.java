667aa7ee7c35b38b1dfb705eb242f95bbe3ea5d8
Atishay Jain <atishayjain23@gmail.com>
2019-11-02 10:20:41 +0530

    Singleton Pattern using enum.

--- src/com/atishay/proxy/design/pattern/SingletonUse.java
+++ src/com/atishay/proxy/design/pattern/SingletonUse.java
@@ -1,5 +1,13 @@
 package com.atishay.proxy.design.pattern;
 
+public class SingletonUse {
+	public static void main(String[] args) {
+		System.out.println(Singleton.getSingleton());
+		System.out.println(Singleton.getSingleton());
+		System.out.println(Singleton.getSingleton());
+	}
+}
+
 enum SingletonEnum {
 	INSTANCE;
 	private Singleton singleton;
@@ -14,15 +22,15 @@ enum SingletonEnum {
 
 }
 
-public class Singleton {
+// no one can extend this class.
+final class Singleton {
 
 	static {
 		SingletonEnum.INSTANCE.setSingleton(new Singleton());
 	}
 
-	// private constructor, no one can initialize the class.
+	// private constructor, no one can initialize the class from outside.
 	private Singleton() {
-
 	}
 
 	public static Singleton getSingleton() {
